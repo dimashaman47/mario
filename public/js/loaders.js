ffbceec80fa0d10d17d01750964c838d787bc5a7
Dmitry Shmanko <d.shmanko@duma-ys.ru>
2020-09-07 14:05:40 +1100

    ep5 31:05

--- public/js/loaders.js
+++ public/js/loaders.js
@@ -12,19 +12,33 @@ export function loadImage(url) {
   });
 }
 
+function createTiles(level, backgrounds) {
+  backgrounds.forEach((background) => {
+    background.ranges.forEach(([x1, x2, y1, y2]) => {
+      for (let x = x1; x < x2; ++x) {
+        for (let y = y1; y < y2; ++y) {
+          level.tiles.set(x, y, {
+            name: background.tile,
+          });
+        }
+      }
+    });
+  });
+}
+
 export function loadLevel(name) {
   return Promise.all([
     loadBackgroundSprites(),
     fetch(`/levels/${name}.json`).then((r) => r.json()),
   ]).then(([backgroundSprite, levelSpec]) => {
     const level = new Level();
-    const backgroundLayer = createBackgroundLayer(
-      levelSpec.backgrounds,
-      backgroundSprite
-    );
+    createTiles(level, levelSpec.backgrounds);
+    const backgroundLayer = createBackgroundLayer(level, backgroundSprite);
     level.comp.layers.push(backgroundLayer);
     const spriteLayer = createSpriteLayer(level.entities);
     level.comp.layers.push(spriteLayer);
+
+    console.log(level);
 
     return level;
   });
